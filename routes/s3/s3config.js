d8f68f9a552f3f468f8c89e2c433d22f089aabd6
minsu <33726146+earlgreyholic@users.noreply.github.com>
2018-05-21 05:31:31 +0000

    Update s3config.js

--- routes/s3/s3config.js
+++ routes/s3/s3config.js
@@ -1,5 +1,5 @@
 module.exports = {
-   region:'ap-northeast-2',
-   accessKeyId:'AKIAJVMBIBWOZQTCILVQ',
-   secretAccessKey:'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'
+   region:'...',
+   accessKeyId:'...',
+   secretAccessKey:'...'
 };
